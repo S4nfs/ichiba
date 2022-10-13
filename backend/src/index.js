571729a84127d549dd630580b2c6daf857766720
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-13 22:20:26 +0530

    individual routes for both admin and user

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
-const userRoutes = require('./routes/user.route');
+// const userRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin/auth.route');
+
 const app = express();
 
 app.use(express.json())
@@ -11,8 +13,9 @@ mongoose.connect('mongodb://localhost:27017/MERN-ichiba').then(() => {
     console.log('Databse connected');
 })
 
-//routes
-app.use('/api', userRoutes)
+//routes 
+// app.use('/api', userRoutes)
+app.use('/api', adminRoutes)
 
 
 app.listen(process.env.PORT, () => {
