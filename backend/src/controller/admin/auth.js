3238401a9d4a7d2c00709b5115da9fe13aa1fc96
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-13 22:18:37 +0530

    admin controller

---
+++ backend/src/controller/admin/auth.js
@@ -0,0 +1,56 @@
+const User = require('../../models/user');
+const jwt = require('jsonwebtoken');
+
+exports.signup = (req, res) => {
+    User.findOne({ email: req.body.email }).exec((error, data) => {
+        if (data) {
+            return res.status(400).json({
+                message: "Admin already registered"
+            });
+        }
+        const { firstName, lastName, email, password } = req.body;
+        const user = new User({ firstName, lastName, email, password, username: Math.random().toString(), role: 'admin' });
+        user.save((error, data) => {
+            if (error) {
+                return res.status(400).json({
+                    message: "Something went wrong !:("
+                });
+            }
+            if (data) {
+                return res.status(201).json({
+                    message: "Admin Created"
+                })
+            }
+        })
+    })
+}
+
+exports.signin = (req, res) => {
+    User.findOne({ email: req.body.email }).exec((error, user) => {
+        if (error) return res.status(400).json({ error });
+        if (user) {
+            if (user.authenticate(req.body.password) && user.role === 'admin') {
+                const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+                const { _id, firstName, lastName, email, role, fullName } = user;
+                res.status(200).json({
+                    token,
+                    user: { _id, firstName, lastName, email, role, fullName }
+                });
+            } else {
+                return res.status(400).json({
+                    message: "Invalid password"
+                });
+            }
+        } else {
+            return res.status(400).json({ message: 'Something went wrong' })
+        }
+    });
+}
+
+
+exports.isAuthenticatedByJWT = (req, res, next) => {
+    const token = req.headers.authorization.split(" ")[1];
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next()
+}
