4730150bba573fe14f7e7382250726dcf744578e
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-13 23:49:51 +0530

    containers for individual components

---
+++ dashboard/src/containers/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const Home = (props) => {
+    return (
+        <Layout>
+            <div className="text-center" style={{ margin: '5rem', background: '#fff' }} >
+                <h1>Admin Dashboard</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti numquam, itaque voluptates blanditiis natus laboriosam sint aut quae a nulla eius perferendis voluptatibus magnam asperiores quasi quas tempore nam? Dolorem.</p>
+            </div>
+        </Layout>
+    )
+}
+
+export default Home
