1ab2330b8727cb4c7b1d7adda08bbfa67ec4f389
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-14 14:29:58 +0530

    signin form

--- dashboard/src/containers/Signin/index.js
+++ dashboard/src/containers/Signin/index.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import Layout from '../../components/Layout'
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
+import Input from '../../components/UI/Input'
 
-const Signin = () => {
+const Signin = (props) => {
     return (
-        <div>Signin</div>
+        <Layout>
+            <Container>
+                <Row style={{ marginTop: '50px' }}>
+                    <Col md={{ span: 6, offset: 3 }}>
+                        <Form>
+                            <Input
+                                label="Email"
+                                type="text"
+                                placeholder="Email"
+                                value=""
+                                onChange={() => { }}
+                            />
+                            <Input
+                                label="Password"
+                                type="password"
+                                placeholder="password"
+                                value=""
+                                onChange={() => { }}
+                            />
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+
+            </Container>
+        </Layout>
     )
 }
 
