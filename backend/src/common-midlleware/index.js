a8434a5e68764c6bf3ffb1b8118346739195ac81
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-14 23:44:33 +0530

    seperate file for JWT authentication for routes

---
+++ backend/src/common-midlleware/index.js
@@ -0,0 +1,6 @@
+exports.isAuthenticatedByJWT = (req, res, next) => {
+    const token = req.headers.authorization.split(" ")[1];
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next()
+}
