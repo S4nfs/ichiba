a5d4885b6c1eefd615e62a3bdc962d55fdc7db8e
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-14 14:30:04 +0530

    signup form

--- dashboard/src/containers/Signup/index.js
+++ dashboard/src/containers/Signup/index.js
@@ -1,8 +1,58 @@
 import React from 'react'
-
-const Signup = () => {
+import Layout from '../../components/Layout'
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
+import Input from '../../components/UI/Input'
+const Signup = (props) => {
     return (
-        <div>Signup</div>
+        <Layout>
+            <Container>
+                <Row style={{ marginTop: '50px' }}>
+                    <Col md={{ span: 6, offset: 3 }}>
+                        <Form>
+                            <Row>
+                                <Col md={6}>
+                                    <Input
+                                        label="First Name"
+                                        type="text"
+                                        placeholder="First name"
+                                        value=""
+                                        onChange={() => { }}
+                                    />
+                                </Col>
+                                <Col md={6}>
+                                    <Input
+                                        label="Last Name"
+                                        type="text"
+                                        placeholder="Last name"
+                                        value=""
+                                        onChange={() => { }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Input
+                                label="Email"
+                                type="text"
+                                placeholder="Email"
+                                value=""
+                                onChange={() => { }}
+                            />
+
+                            <Input
+                                label="Password"
+                                type="password"
+                                placeholder="password"
+                                value=""
+                                onChange={() => { }}
+                            />
+
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+        </Layout >
     )
 }
 
