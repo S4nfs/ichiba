d7dc1cb55e253aa8945a367fbc5b09c238816352
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-15 15:05:39 +0530

    jwt authenticated routes

--- backend/src/routes/category.js
+++ backend/src/routes/category.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { isAuthenticatedByJWT, adminMiddleware } = require('../common-middleware/index');
 const { addCategory, getCategories } = require('../controller/category');
 const router = express.Router();
 
-router.post('/category/create', addCategory);
+router.post('/category/create', isAuthenticatedByJWT, adminMiddleware, addCategory);
 router.get('/category/getcategory', getCategories);
 
 
