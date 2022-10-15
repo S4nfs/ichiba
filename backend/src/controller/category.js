ba5aaee699a33b6e1ee92c5818507c591c601f88
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-15 15:05:11 +0530

    creating sub-categories

--- backend/src/controller/category.js
+++ backend/src/controller/category.js
@@ -1,12 +1,32 @@
 const Category = require('../models/category');
 const slugify = require('slugify');
 
+//recusrive function to add sub-categories inside its parent
+function createCategories(categories, parentId = null) {
+    const categoryList = [];
+    let category;
+    if (parentId == null) {
+        category = categories.filter(cat => cat.parentId == undefined)
+    } else {
+        category = categories.filter(cat => cat.parentId == parentId)
+    }
+    for (let c of category) {
+        categoryList.push({
+            _id: c._id,
+            name: c.name,
+            slug: c.sulg,
+            childern: createCategories(categories, c._id)
+        })
+    }
+    return categoryList;
+}
+
 exports.addCategory = (req, res) => {
     const categoryObj = {
         name: req.body.name,
         slug: slugify(req.body.name)
     }
-    if (req.body.productId) {   //if parentId exists
+    if (req.body.parentId) {   //if parentId exists
         categoryObj.parentId = req.body.parentId
     }
     const cat = new Category(categoryObj);  //save category
@@ -19,6 +39,9 @@ exports.addCategory = (req, res) => {
 exports.getCategories = (req, res) => {
     Category.find({}).exec((error, categories) => {
         if (error) return res.status(400).json({ error: error });
-        if (categories) return res.status(400).json({ categories: categories });
+        if (categories) {
+            const categoryList = createCategories(categories); //recusrively adding sub-categories inside its parent
+            return res.status(400).json({ categoryList: categoryList });
+        }
     })
 }
