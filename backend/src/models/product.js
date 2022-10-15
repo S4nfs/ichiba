eec406f5fbfec12cb6261f2346fbad307d1d7dab
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-15 22:31:19 +0530

    product schema

---
+++ backend/src/models/product.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    offer: {
+        type: Number
+    },
+    productPictures: [{
+        img: {
+            type: String
+        }
+    }],
+    reviews: [{
+        userid: { type: mongoose.SchemaTypes.ObjectId, ref: 'User' }, //relationships
+        reviews: String
+    }],
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true, },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, },
+    updatedAt: Date,
+}, { timestamps: true })
+
+module.exports = mongoose.model('Product', productSchema);
