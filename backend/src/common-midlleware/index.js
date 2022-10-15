1a0137e6ac589fc3e8d348e7553f940b561d8325
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-15 15:04:23 +0530

    del

--- backend/src/common-midlleware/index.js
+++
@@ -1,6 +0,0 @@
-exports.isAuthenticatedByJWT = (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1];
-    const user = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = user;
-    next()
-}
