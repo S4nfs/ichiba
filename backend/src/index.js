4c2869f6e4281ad1e3baef23d3e5a5da4f4d8424
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-16 23:22:23 +0530

    exposing public/uploads to global access

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,13 @@ const userRoutes = require('./routes/auth.route');
 const adminRoutes = require('./routes/admin/auth.route');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
-
+const cartRoutes = require('./routes/cart');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json())
+app.use('/public', express.static(path.join(__dirname, 'uploads')));   //expose uploads to public
 
 //database
 mongoose.connect('mongodb://localhost:27017/MERN-ichiba').then(() => {
@@ -21,6 +23,8 @@ app.use('/api', userRoutes);
 app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
+app.use('/api', cartRoutes);
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening at port ${process.env.PORT}`)
