8af4165e7dd0ed595d05c277f85941c0db5b77a0
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-16 23:21:40 +0530

    multer handling category image uploads

--- backend/src/routes/category.js
+++ backend/src/routes/category.js
@@ -2,8 +2,23 @@ const express = require('express');
 const { isAuthenticatedByJWT, adminMiddleware } = require('../common-middleware/index');
 const { addCategory, getCategories } = require('../controller/category');
 const router = express.Router();
+const multer = require('multer');
+const shortid = require('shortid');
+const path = require('path');
 
-router.post('/category/create', isAuthenticatedByJWT, adminMiddleware, addCategory);
+//multer to store uploads and generation shortid
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(path.dirname(__dirname), 'uploads'))
+    },
+    filename: function (req, file, cb) {
+        cb(null, shortid.generate() + '-' + file.originalname)
+    }
+})
+const upload = multer({ storage });
+
+
+router.post('/category/create', isAuthenticatedByJWT, adminMiddleware, upload.single('categoryImage'), addCategory);
 router.get('/category/getcategory', getCategories);
 
 
