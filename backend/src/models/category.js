f1eb59c43c1a7ae0c0b30346a65102649aad2119
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-16 23:21:09 +0530

    added image to category schema

--- backend/src/models/category.js
+++ backend/src/models/category.js
@@ -11,6 +11,9 @@ const categorySchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    categoryImage: {
+        type: String,
+    },
     parentId: {
         type: String
     },
