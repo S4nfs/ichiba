e6578c9edac2684c576a4a35803e9156bd6ee039
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-17 23:17:38 +0530

    redux store

---
+++ dashboard/src/store/index.js
@@ -0,0 +1,7 @@
+import { applyMiddleware, legacy_createStore as createStore } from 'redux'
+import rootReducer from '../reducers';
+import thunk from 'redux-thunk'
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+export default store;
