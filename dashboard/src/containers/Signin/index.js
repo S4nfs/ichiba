b70ef1850461ebf26f5c6a2265d5bd8d832ba938
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-17 23:17:11 +0530

    redux container

--- dashboard/src/containers/Signin/index.js
+++ dashboard/src/containers/Signin/index.js
@@ -2,14 +2,27 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 import Input from '../../components/UI/Input'
+import { login } from '../../actions'
+import { useDispatch } from 'react-redux';
 
 const Signin = (props) => {
+
+    const dispatch = useDispatch();
+
+    const userLogin = (e) => {
+        e.preventDefault();
+        const user = {
+            email: 'sagar@gmail.com',
+            password: 'tt'
+        }
+        dispatch(login(user))
+    }
     return (
         <Layout>
             <Container>
                 <Row style={{ marginTop: '50px' }}>
                     <Col md={{ span: 6, offset: 3 }}>
-                        <Form>
+                        <Form onSubmit={userLogin}>
                             <Input
                                 label="Email"
                                 type="text"
