cf1cd7efc2a7f0302d64336278e52491fc43e4f3
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-20 22:23:21 +0530

    signin action

--- dashboard/src/actions/auth.action.js
+++ dashboard/src/actions/auth.action.js
@@ -1,12 +1,31 @@
+import axios from "../helpers/axios"
 import { authConstants } from "./constants"
 
 export const login = (user) => {
+
     return async (dispatch) => {
-        dispatch({
-            type: authConstants.LOGIN_REQUEST,
-            payload: {
-                ...user
+        dispatch({ type: authConstants.LOGIN_REQUEST })
+        const res = await axios.post('/admin/signin', {
+            ...user
+        });
+        if (res.status === 200) {
+            const { token, user } = res.data;
+            localStorage.setItem('token', token);
+            dispatch({
+                type: authConstants.LOGIN_SUCCESS,
+                payload: {
+                    token, user
+                }
+            })
+        } else {
+            if (res.status === 400) {
+                dispatch({
+                    type: authConstants.LOGIN_FAILURE,
+                    payload: {
+                        error: res.data.error
+                    }
+                })
             }
-        })
+        }
     }
 }
