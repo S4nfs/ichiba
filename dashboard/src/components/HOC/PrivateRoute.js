ba8e49e0d547f15c3f57e4e27be21706e028cd08
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-23 23:37:31 +0530

    privateRoute for Children components

--- dashboard/src/components/HOC/PrivateRoute.js
+++ dashboard/src/components/HOC/PrivateRoute.js
@@ -2,16 +2,13 @@ import React from 'react'
 import { Route, Navigate } from 'react-router-dom'
 
 //private routes after successful authentication like home
-const PrivateRoute = ({ component: Component, ...rest }) => {   //destructure props
-    return <Route {...rest} component={(props) => {
-        const token = window.localStorage.getItem('token');
-        if (token) {
-            return <Component {...props} />
-        } else {
-            return <Navigate to={'/signin'} />
-        }
+const PrivateRoute = ({ children }) => {
+    const token = window.localStorage.getItem('token');
+    if (token) {
+        return children;
+    } else {
+        return <Navigate to="/signin" />
     }
-    } />
 }
 
 export default PrivateRoute;
