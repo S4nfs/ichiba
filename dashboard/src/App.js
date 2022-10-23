665bf29dfaa75ed674272fc84330e88781eb4782
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-23 23:40:11 +0530

    authenticating all routes

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,23 +1,31 @@
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
 import Home from './containers/Home';
 import Signin from './containers/Signin';
 import Signup from './containers/Signup';
 import PrivateRoute from './components/HOC/PrivateRoute';
+import { useDispatch, useSelector } from 'react-redux';
+import { isUserLoggedIn } from './actions';
 
 function App() {
+  const dispatch = useDispatch()
+  const auth = useSelector(state => state.auth); //get store value
+
+  useEffect(() => {
+    if (!auth.authenticate) {
+      dispatch(isUserLoggedIn());
+    }
+  }, [])
+
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route exact element={<PrivateRoute />}>
-            <Route exact path="/" element={<Home />} />       {/* sub-route home */}
-          </Route>
-          <Route path='/signin' element={<Signin />}></Route>
-          <Route path='/signup' element={<Signup />}></Route>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route path='/signin' element={<Signin />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+      </Routes>
     </div>
   );
 }
