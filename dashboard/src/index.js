7453611e65fa2e9c0cbc0244427e217d589561df
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-23 23:39:41 +0530

    wrapping app component inside Router for birds-eye authentication

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 window.store = store;
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
   </Provider>
 
 );
