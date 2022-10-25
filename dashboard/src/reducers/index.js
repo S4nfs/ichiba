4834e4c93403cdad48cf2a29d44b5f262c1259e5
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-25 19:50:58 +0530

    reducing functions into a single reducing function

--- dashboard/src/reducers/index.js
+++ dashboard/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth.reducer";
+import userReducer from "./user.reducer";
 
 const rootReducer = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    user: userReducer
+
 })
 
 export default rootReducer;
