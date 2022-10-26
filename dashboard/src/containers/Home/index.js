af70dcc864592eb2603c98499bca9252fdd55c7e
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-26 12:02:26 +0530

    sidebar container

--- dashboard/src/containers/Home/index.js
+++ dashboard/src/containers/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Row, Col, Container } from 'react-bootstrap'
+import { NavLink } from "react-router-dom";
 import Layout from '../../components/Layout'
 import './style.css'
 
@@ -8,7 +9,15 @@ const Home = (props) => {
         <Layout>
             <Container fluid>
                 <Row>
-                    <Col md={2} className="sidebar">Side Bar</Col>
+                    <Col md={2} className="sidebar">
+                        <ul>
+                            <li><NavLink to={'/'}>Home</NavLink></li>
+                            <li><NavLink to={'/products'}>Products</NavLink></li>
+                            <li><NavLink to={'/orders'}>Orders</NavLink></li>
+
+
+                        </ul>
+                    </Col>
                     <Col md={10} style={{ marginLeft: 'auto' }}>Container</Col>
                 </Row>
             </Container>
