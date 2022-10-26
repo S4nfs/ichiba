5b78778127d7ba849f22eef98ccc8b8727714874
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-26 12:02:49 +0530

    product component

---
+++ dashboard/src/containers/Products/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const Products = () => {
+    return (
+        <div>
+            <Layout sidebar>
+                Products
+            </Layout>
+
+        </div>
+    )
+}
+
+export default Products
