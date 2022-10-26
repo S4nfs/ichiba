92e6104875f5bc2af9be437fda816a30a1d3cf25
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-26 12:02:38 +0530

    order component

---
+++ dashboard/src/containers/Orders/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const Orders = () => {
+    return (
+        <div>
+            <Layout sidebar>
+                Orders
+            </Layout>
+
+        </div>
+    )
+}
+
+export default Orders
