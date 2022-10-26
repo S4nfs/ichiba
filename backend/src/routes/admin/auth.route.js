d54ef0e241f0262a6bf15bbe57d401a8d467f055
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-26 12:01:21 +0530

    signout route

--- backend/src/routes/admin/auth.route.js
+++ backend/src/routes/admin/auth.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { signup, signin } = require('../../controller/admin/auth')
+const { isAuthenticatedByJWT } = require('../../common-middleware');
+const { signup, signin, signout } = require('../../controller/admin/auth')
 const { validateSignupRequest, validateSigninRequest, isRequestValidated } = require('../../validators/auth');
 const router = express.Router();
 
 router.post('/admin/signup', validateSignupRequest, isRequestValidated, signup);
 router.post('/admin/signin', validateSigninRequest, isRequestValidated, signin);
+router.post('/admin/signout', isAuthenticatedByJWT, signout);
 
 
 
