116f2b6588d6e1a29c090fe23a61356ced150aa9
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-27 22:23:02 +0530

    reducerr for category

---
+++ dashboard/src/reducers/category.reducer.js
@@ -0,0 +1,20 @@
+import { categoryConstants } from "../actions/constants"
+
+const initState = {
+    categories: [],
+    loading: false,
+    error: null
+}
+
+export default (state = initState, action) => {
+    switch (action.type) {
+        case categoryConstants.GET_ALL_CATEGORIES_SUCCESS:
+            state = {
+                ...state,
+                categories: action.payload.categories
+            }
+            break;
+    }
+
+    return state;
+}
