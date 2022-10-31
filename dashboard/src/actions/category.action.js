e5e40441aca3b57cdffc35c3e62b3c1108574e5a
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-31 22:34:21 +0530

    category actions

--- dashboard/src/actions/category.action.js
+++ dashboard/src/actions/category.action.js
@@ -25,3 +25,21 @@ export const getAllCategory = () => {
         }
     }
 }
+
+export const addCategory = (form) => {
+    return async (dispatch) => {
+        dispatch({ type: categoryConstants.ADD_NEW_CATEGORY_REQUEST })
+        const res = await axios.post('/category/create', form);
+        if (res.status === 200) {
+            dispatch({
+                type: categoryConstants.ADD_NEW_CATEGORY_SUCCESS,
+                payload: { category: res.data.category }
+            })
+        } else {
+            dispatch({
+                type: categoryConstants.ADD_NEW_CATEGORY_FAILURE,
+                payload: res.data.error
+            })
+        }
+    }
+}
