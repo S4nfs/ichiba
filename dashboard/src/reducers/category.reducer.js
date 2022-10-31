f870b5838db705923f4636a83a98b0c3ca24b8ff
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-31 22:33:37 +0530

    category reducer

--- dashboard/src/reducers/category.reducer.js
+++ dashboard/src/reducers/category.reducer.js
@@ -6,12 +6,57 @@ const initState = {
     error: null
 }
 
+const buildNewCategories = (parentId, categories, category) => {
+    let myCategories = [];
+    for (let cat of categories) {
+        if (cat._id == parentId) {
+            myCategories.push({
+                ...cat,
+                children: cat.children && cat.children.length > 0 ? buildNewCategories(parentId, [...cat.children, {
+                    _id: category.id,
+                    name: category.name,
+                    slug: category.slug,
+                    parentId: category.parentId,
+                    children: category.children
+                }], category) : []
+            })
+        } else {
+            myCategories.push({
+                ...cat,
+                children: cat.children && cat.children.length > 0 ? buildNewCategories(parentId, cat.children, category) : []
+            })
+        }
+    }
+    return myCategories;
+}
+
 export default (state = initState, action) => {
     switch (action.type) {
         case categoryConstants.GET_ALL_CATEGORIES_SUCCESS:
             state = {
                 ...state,
                 categories: action.payload.categories
+            }
+            break;
+        case categoryConstants.ADD_NEW_CATEGORY_REQUEST:
+            state = {
+                ...state,
+                loading: true,
+            }
+            break;
+        case categoryConstants.ADD_NEW_CATEGORY_SUCCESS:
+            const category = action.payload.category
+            const updatedCategories = buildNewCategories(category.parentId, state.categories, category);
+            console.log(updatedCategories)
+            state = {
+                ...state,
+                categories: updatedCategories,
+                loading: false,
+            }
+            break;
+        case categoryConstants.ADD_NEW_CATEGORY_FAILURE:
+            state = {
+                ...initState
             }
             break;
     }
