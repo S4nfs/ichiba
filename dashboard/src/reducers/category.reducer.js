a3cbee0111f02991d252571bacc417d04b58c3f1
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-01 23:15:04 +0530

    logic building

--- dashboard/src/reducers/category.reducer.js
+++ dashboard/src/reducers/category.reducer.js
@@ -10,8 +10,11 @@ const buildNewCategories = (parentId, categories, category) => {
     let myCategories = [];
     for (let cat of categories) {
         if (cat._id == parentId) {
+            /* ex. lets say we create a category in kitchen portion so, if (state.categories._id===parentId) then cat here will be
+               { _id: '6349a56cbf58ef7d83f87629', name: 'Kitchen', children: [] }
+            */
             myCategories.push({
-                ...cat,
+                ...cat,             //take ...rest(operator) of contents and push the {object} into children [array]
                 children: cat.children && cat.children.length > 0 ? buildNewCategories(parentId, [...cat.children, {
                     _id: category.id,
                     name: category.name,
@@ -45,7 +48,7 @@ export default (state = initState, action) => {
             }
             break;
         case categoryConstants.ADD_NEW_CATEGORY_SUCCESS:
-            const category = action.payload.category
+            const category = action.payload.category;   //payload
             const updatedCategories = buildNewCategories(category.parentId, state.categories, category);
             console.log(updatedCategories)
             state = {
