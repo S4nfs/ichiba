60947ba24f6f78ab6e0eb414366b35f4d49700d0
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-03 21:12:37 +0530

    extend jwt expire time

--- backend/src/controller/admin/auth.js
+++ backend/src/controller/admin/auth.js
@@ -30,9 +30,9 @@ exports.signin = (req, res) => {
         if (error) return res.status(400).json({ error });
         if (user) {
             if (user.authenticate(req.body.password) && user.role === 'admin') {
-                const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+                const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '20d' });
                 const { _id, firstName, lastName, email, role, fullName } = user;
-                res.cookie('token', token, { expiresIn: '1h' }) //set cookie on server
+                res.cookie('token', token, { expiresIn: '20d' }) //set cookie on server
                 res.status(200).json({
                     token: token,
                     user: { _id, firstName, lastName, email, role, fullName }
