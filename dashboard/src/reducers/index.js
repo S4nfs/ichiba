e4f0f098f7c59d43769635e82fcedda8f5bb7d31
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-04 23:08:13 +0530

    root reducer updated

--- dashboard/src/reducers/index.js
+++ dashboard/src/reducers/index.js
@@ -1,17 +1,17 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth.reducer";
 import userReducer from "./user.reducer";
-// import orderReducer from "./order.reducer";
-// import productReducer from "./product.reducer";
 import categoryReducer from "./category.reducer";
+import productReducer from "./product.reducer";
+// import orderReducer from "./order.reducer";
 
 
 const rootReducer = combineReducers({
     auth: authReducer,
     user: userReducer,
     category: categoryReducer,
+    product: productReducer,
     // order: orderReducer,
-    // product: productReducer
 
 })
 
