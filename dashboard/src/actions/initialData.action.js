da8e677c21795eff5022b4f31dfff3281cb90f9a
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-04 23:04:37 +0530

    initialData action for product and categories

---
+++ dashboard/src/actions/initialData.action.js
@@ -0,0 +1,20 @@
+import { categoryConstants, productConstants } from "./constants"
+import axios from "../helpers/axios"
+
+export const getinitialData = () => {
+    return async dispatch => {
+        const res = await axios.post('/initialdata');
+        if (res.status === 200) {
+            const { categories, products } = res.data
+            dispatch({
+                type: categoryConstants.GET_ALL_CATEGORIES_SUCCESS,
+                payload: { categories }
+            });
+            dispatch({
+                type: productConstants.GET_ALL_PRODUCTS_SUCCESS,
+                payload: { products }
+            });
+        }
+        console.log(res);
+    }
+}
