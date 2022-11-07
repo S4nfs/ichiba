34e25e6c54e7bcda81fed857cae79b630e2a2f05
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-07 23:14:34 +0530

    root reducer

---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import categoryReducer from "./category.reducer";
+
+
+const rootReducer = combineReducers({
+    category: categoryReducer,
+
+})
+
+export default rootReducer;
