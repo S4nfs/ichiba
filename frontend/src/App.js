7da381a7632074f4f292caaa91104c8a7e6c5d80
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-09 23:07:30 +0530

    Routes for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import HomePage from './containers/HomePage';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProductListPage from './containers/ProductListPage';
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/:slug' element={<ProductListPage />} />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
