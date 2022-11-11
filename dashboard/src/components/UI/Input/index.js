3daa0ecc4cab4a9515144be2d856825085c7a73f
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-11 19:33:35 +0530

    label

--- dashboard/src/components/UI/Input/index.js
+++ dashboard/src/components/UI/Input/index.js
@@ -4,7 +4,7 @@ import { Form } from 'react-bootstrap';
 const Input = (props) => {
     return (
         <Form.Group className="mb-3" controlId="">
-            <Form.Label>{props.label}</Form.Label>
+            {props.label && <Form.Label>{props.label}</Form.Label>}
             <Form.Control type={props.type} placeholder={props.placeholder} value={props.value} onChange={props.onChange} />
             <Form.Text className="text-muted">
                 {props.errorMessage}
