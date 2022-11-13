5178e9932a72a9059b298042966169bfb2c470f0
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-13 23:11:05 +0530

    redux-logger middleware

--- dashboard/src/store/index.js
+++ dashboard/src/store/index.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, legacy_createStore as createStore } from 'redux'
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
