8be3ca7f6c29b73ef5ef5b58ff2051493d754df9
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-28 22:22:14 +0530

    route for delete category

--- backend/src/routes/category.js
+++ backend/src/routes/category.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { isAuthenticatedByJWT, adminMiddleware } = require('../common-middleware/index');
-const { addCategory, getCategories, updateCategories } = require('../controller/category');
+const { addCategory, getCategories, updateCategories, deleteCategories } = require('../controller/category');
 const router = express.Router();
 const multer = require('multer');
 const shortid = require('shortid');
@@ -21,6 +21,7 @@ const upload = multer({ storage });
 router.post('/category/create', isAuthenticatedByJWT, adminMiddleware, upload.single('categoryImage'), addCategory);
 router.get('/category/getcategory', getCategories);
 router.post('/category/update', upload.array('categoryImage'), updateCategories);
+router.post('/category/delete', deleteCategories);
 
 
 module.exports = router;
