498b2f1fce1bbc72e9bfd4a83fa7b6272f138b94
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-29 22:55:18 +0530

    axios interceptors to handle error response and req headers

--- dashboard/src/helpers/axios.js
+++ dashboard/src/helpers/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { api } from '../urlConfig';
+import { store } from '../store';
+import { authConstants } from '../actions/constants';
+
 const token = window.localStorage.getItem('token')
 const axiosInstance = axios.create({
     baseURL: api,
@@ -7,5 +10,27 @@ const axiosInstance = axios.create({
         'Authorization': token ? `Bearer ${token}` : ''
     }
 });
+
+//axios middlewares - interceptors (You can intercept requests or responses before they are handled by then or catch)
+axiosInstance.interceptors.request.use((req) => {
+    const { auth } = store.getState();
+    if (auth.token) {
+        req.headers.Authorization = `Bearer ${auth.token}`;    //refresh token if we get status 500/400 from below 👇
+    }
+    return req;
+})
+
+//dispatch LOUGOUT_SUCCESS action if we got JWT expired or something similar to status 500/400
+axios.interceptors.response.use(() => {
+    return res
+}, (error) => {
+    console.log(error);
+    const { status } = error.response;
+    if (status === 500 || status === 400) {
+        localStorage.clear()
+        store.dispatch({ type: authConstants.LOGOUT_SUCCESS })
+    }
+    return Promise.reject(error)
+})
 
 export default axiosInstance
