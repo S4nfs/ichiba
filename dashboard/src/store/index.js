c8ae23f7d7c72ffa46f594227480d9b81f060b24
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-30 23:01:05 +0530

    redux devtool to debug state

--- dashboard/src/store/index.js
+++ dashboard/src/store/index.js
@@ -2,7 +2,8 @@ import { applyMiddleware, legacy_createStore as createStore } from 'redux'
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store;
