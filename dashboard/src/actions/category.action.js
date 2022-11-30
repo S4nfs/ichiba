e26f20861d94c286c599091f288724e43499d6b9
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-30 22:58:27 +0530

    dispatch actions for updateCategories

--- dashboard/src/actions/category.action.js
+++ dashboard/src/actions/category.action.js
@@ -29,30 +29,40 @@ export const getAllCategory = () => {
 export const addCategory = (form) => {
     return async (dispatch) => {
         dispatch({ type: categoryConstants.ADD_NEW_CATEGORY_REQUEST })
-        const res = await axios.post('/category/create', form);
-        if (res.status === 200) {
-            dispatch({
-                type: categoryConstants.ADD_NEW_CATEGORY_SUCCESS,
-                payload: { category: res.data.category }    //reflect changes as soon as you add category
-            })
-        } else {
-            dispatch({
-                type: categoryConstants.ADD_NEW_CATEGORY_FAILURE,
-                payload: res.data.error
-            })
+        try {
+            const res = await axios.post('/category/create', form);
+            if (res.status === 200) {
+                dispatch({
+                    type: categoryConstants.ADD_NEW_CATEGORY_SUCCESS,
+                    payload: { category: res.data.category }    //reflect changes as soon as you add category
+                })
+            } else {
+                dispatch({
+                    type: categoryConstants.ADD_NEW_CATEGORY_FAILURE,
+                    payload: res.data.error
+                })
+            }
+        } catch (error) {
+            console.log(error.response)
         }
+
     }
 }
 
 
 export const updateCategories = (form) => {
     return async (dispatch) => {
+        dispatch({ type: categoryConstants.UPDATE_CATEGORY_REQUEST })
         const res = await axios.post('/category/update', form);
         if (res.status === 201) {
-            return true;
-            console.log(res)
+            dispatch({ type: categoryConstants.UPDATE_CATEGORY_SUCCESS })
+            dispatch(getAllCategory());
         } else {
-            console.log(res)
+            const { error } = res;
+            dispatch({
+                type: categoryConstants.UPDATE_CATEGORY_FAILURE,
+                payload: { error }
+            })
         }
     }
 }
@@ -71,3 +81,4 @@ export const deleteCategories = (ids) => {
         }
     }
 }
+
