fb3c3af19bb0bc50484d0f4783554b769127a16c
S4nfs <zyzzaestheticsgods@gmail.com>
2022-12-05 23:55:25 +0530

    middlewares for auth

--- backend/src/routes/admin/initialData.js
+++ backend/src/routes/admin/initialData.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { isAuthenticatedByJWT, adminMiddleware } = require('../../common-middleware');
 const { initialData } = require('../../controller/admin/initialData');
 
 const router = express.Router();
 
-router.post('/initialdata', initialData);
+router.post('/initialdata', isAuthenticatedByJWT, adminMiddleware, initialData);
 
 
 
