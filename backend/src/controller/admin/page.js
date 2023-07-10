13a6f3761d749b288fac1be1ea900570a08c6088
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-10 23:31:46 +0530

    undefined

--- backend/src/controller/admin/page.js
+++ backend/src/controller/admin/page.js
@@ -2,13 +2,13 @@ const Page = require("../../models/page");
 
 exports.createPage = (req, res) => {
     const { banners, products } = req.files;
-    if (banners.length > 0) {
+    if (banners && banners.length > 0) {
         req.body.banners = banners.map((banner, index) => ({
             img: `${process.env.API}/public/${banner.filename}`,
             navigateTo: `/bannerClicked?categoryId=${req.body.category}&type=${req.body.type}`
         }))
     }
-    if (products.length > 0) {
+    if (products && products.length > 0) {
         req.body.products = products.map((product, index) => ({
             img: `${process.env.API}/public/${product.filename}`,
             navigateTo: `/productClicked?categoryId=${req.body.category}&type=${req.body.type}`
@@ -41,7 +41,7 @@ exports.createPage = (req, res) => {
 
 exports.getPage = (req, res) => {
     const { category, type } = req.params;
-    if (typs === "page") {
+    if (type === "page") {
         Page.findOne({ category: category }).exec((error, page) => {
             if (error) return res.status(400).json({ error: error });
             if (page) return res.status(201).json({ page });
