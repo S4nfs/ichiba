0ad041edd8dc39b171cacf35ebe6ab5586d9484d
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-12 23:56:43 +0530

    dev tools added

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, legacy_createStore as createStore } from 'redux'
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store;
