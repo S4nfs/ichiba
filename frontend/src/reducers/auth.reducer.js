f9572dc3882872e1c16dea8cf378fe6ed4f0a9f7
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-13 14:48:56 +0530

    auth reducer

---
+++ frontend/src/reducers/auth.reducer.js
@@ -0,0 +1,55 @@
+import { authConstants } from "../actions/constants"
+
+const initstate = {
+    token: null,
+    user: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        picture: '',
+    },
+    authenticate: false,
+    authenticating: false,
+    loading: false,
+    error: null,
+    message: ''
+
+}
+export default (state = initstate, action) => {
+    switch (action.type) {
+        case authConstants.LOGIN_REQUEST:
+            state = {
+                ...state,
+                authenticating: true
+            }
+            break;
+        case authConstants.LOGIN_SUCCESS:
+            state = {
+                ...state,
+                user: action.payload.user,
+                token: action.payload.token,
+                authenticate: true,
+                authenticating: false
+            }
+            break;
+        case authConstants.LOGOUT_REQUEST:
+            state = {
+                ...state,
+                loading: true
+            }
+            break;
+        case authConstants.LOGOUT_SUCCESS:
+            state = {
+                ...initstate
+            }
+            break;
+        case authConstants.LOGOUT_FAILURE:
+            state = {
+                ...state,
+                error: action.payload.error,
+                loading: false
+            }
+            break;
+    }
+    return state
+}
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import categoryReducer from "./category.reducer";
 import productReducer from "./product.reducer";
+import authReducer from "./auth.reducer";
 
 
 const rootReducer = combineReducers({
     category: categoryReducer,
-    product: productReducer
+    product: productReducer,
+    auth: authReducer
 
 })
 
