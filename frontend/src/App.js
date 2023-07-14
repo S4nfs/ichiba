9161fab4d2afda811fc7ab5f89691447038fb26a
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-14 21:59:53 +0530

    authenticate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 import HomePage from './containers/HomePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ProductListPage from './containers/ProductListPage';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { isUserLoggedIn } from './actions';
+import ProductDetailsPage from './containers/ProductDetailsPage';
 function App() {
+  const dispatch = useDispatch()
+  const auth = useSelector(state => state.auth)
+  useEffect(() => {
+    if (!auth.authenticate) {
+      dispatch(isUserLoggedIn())
+    }
+  }, [auth.authenticate])
+
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/:slug' element={<ProductListPage />} />
+          <Route path='/:productSlug/:productId/p' element={<ProductDetailsPage />} />
 
         </Routes>
       </Router>
