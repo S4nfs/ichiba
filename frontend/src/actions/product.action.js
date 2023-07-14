5ebb69c62f5802f9eb36033d5636d764c1ee2dee
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-14 20:28:58 +0530

    product action

--- frontend/src/actions/product.action.js
+++ frontend/src/actions/product.action.js
@@ -18,25 +18,45 @@ export const getProductsBySlug = (slug) => {
 
 export const getProductPage = (payload) => {
     return async dispatch => {
+        dispatch({ type: productConstants.GET_PRODUCT_PAGE_REQUEST })
+        let res;
         try {
             const { cid, type } = payload.searchParams;
-            const res = await axios.get(`/page/${cid}/${type}`);
+            res = await axios.get(`/page/${cid}/${type}`);
             console.log(cid, type)
-            dispatch({ type: productConstants.GET_PRODUCT_PAGE_REQUEST })
-            if (res.status == 200) {
-                const { page } = res.data
-                dispatch({
-                    type: productConstants.GET_PRODUCT_PAGE_SUCCESS,
-                    payload: { page }
-                })
-            } else {
-                const { error } = res.data
-                dispatch({
-                    type: productConstants.GET_PRODUCT_PAGE_FAILURE,
-                    error: { error }
-                })
-            }
+            const { page } = res.data
+            dispatch({
+                type: productConstants.GET_PRODUCT_PAGE_SUCCESS,
+                payload: { page }
+            })
+
         } catch (error) {
+            dispatch({
+                type: productConstants.GET_PRODUCT_PAGE_FAILURE,
+                payload: { error: res.data.error }
+            })
+        }
+
+    }
+}
+
+export const getProductDetailsById = (payload) => {
+    return async dispatch => {
+        dispatch({ type: productConstants.GET_PRODUCT_DETAILS_BY_ID_REQUEST })
+        let res;
+        try {
+            const { productId } = payload.params;
+            res = await axios.get(`/product/${productId}`);
+            dispatch({
+                type: productConstants.GET_PRODUCT_DETAILS_BY_ID_SUCCESS,
+                payload: { productDetails: res.data.product }
+            })
+
+        } catch (error) {
+            dispatch({
+                type: productConstants.GET_PRODUCT_DETAILS_BY_ID_FAILURE,
+                payload: { error: res.data.error }
+            })
         }
 
     }
