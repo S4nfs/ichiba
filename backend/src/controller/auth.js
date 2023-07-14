f18971266aed8a9b0078dc48a6cdc9480cbd7f7d
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-14 20:30:15 +0530

    fix undefined

--- backend/src/controller/auth.js
+++ backend/src/controller/auth.js
@@ -34,7 +34,7 @@ exports.signin = (req, res) => {
     User.findOne({ email: req.body.email }).exec((error, user) => {
         if (error) return res.status(400).json({ error });
         if (user) {
-            if (user.authenticate(req.body.password)) {
+            if (user.authenticate(req.body.password) && user.role === 'user') {
                 const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '20d' });
                 const { _id, firstName, lastName, email, role, fullName } = user;
                 res.status(200).json({
@@ -43,7 +43,7 @@ exports.signin = (req, res) => {
                 });
             } else {
                 return res.status(400).json({
-                    message: "Invalid password"
+                    message: "Invalid details/password"
                 });
             }
         } else {
