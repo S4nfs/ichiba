a06793f3fcfde82205039187f1de3f2e7f3391a2
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-15 23:15:03 +0530

    cart reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from "redux";
 import categoryReducer from "./category.reducer";
 import productReducer from "./product.reducer";
 import authReducer from "./auth.reducer";
+import cartReducer from "./cart.reducer";
 
 
 const rootReducer = combineReducers({
     category: categoryReducer,
     product: productReducer,
-    auth: authReducer
+    auth: authReducer,
+    cart: cartReducer
 
 })
 
