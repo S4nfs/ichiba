be509e0b08d28128b24828de448891dee3cfb0c1
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-16 23:11:26 +0530

    cart page

--- frontend/src/components/CartPage/index.js
+++ frontend/src/components/CartPage/index.js
@@ -1,29 +1,24 @@
 import React from 'react'
 import Card from '../UI/Card'
 import Layout from '../Layout'
+import { useSelector } from 'react-redux'
+import CartItem from './CartItem'
 
 const CartPage = (props) => {
+    const cart = useSelector(state => state.cart)
+    const cartItems = cart.cartItems
     return (
         <Layout>
             <div className="cartContainer">
-                <Card headerLeft={'My Cart'} headerRight={<div>
-                    Deliver To
-                </div>}>
+                <Card headerLeft={'My Cart'} headerRight={<div>Deliver To</div>}>
+                    {
+                        Object.keys(cartItems).map((key, index) => <div className="flexRow">
+                            <CartItem key={index} cartItem={cartItems[key].qty} />
 
-                    <div className="flexRow">
-                        <div className="cartProductContainer">
-                            <img src="" alt="" srcset="" />
                         </div>
-                        <div className="cartItemDetails">
+                        )
+                    }
 
-                            <div>
-                                Product Name
-                            </div>
-                            <div>
-                                Deliver in 4 days
-                            </div>
-                        </div>
-                    </div>
                 </Card >
                 <Card style={{ width: '500px' }}>Price</Card>
             </div >
