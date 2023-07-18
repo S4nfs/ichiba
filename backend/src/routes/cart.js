9854a51d78c63d927af1f7d7fcb89e9aad578ce9
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-18 23:45:16 +0530

    routes updated

--- backend/src/routes/cart.js
+++ backend/src/routes/cart.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { isAuthenticatedByJWT, userMiddleware } = require('../common-middleware/index');
-const { addItemToCart } = require('../controller/cart');
+const { addItemToCart, getCartItems } = require('../controller/cart');
 const router = express.Router();
 
 router.post('/user/cart/addtocart', isAuthenticatedByJWT, userMiddleware, addItemToCart);
+router.post('/user/getCartItems', isAuthenticatedByJWT, userMiddleware, getCartItems);
 
 
 module.exports = router;
