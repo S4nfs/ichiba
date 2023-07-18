110296f8c8292a3b0c6104f38e4f1c4f51cb87f8
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-18 23:44:41 +0530

    handling cartitems

--- backend/src/controller/cart.js
+++ backend/src/controller/cart.js
@@ -1,36 +1,46 @@
 const Cart = require('../models/cart');
 
+function runUpdate(condition, updateData) {
+    return new Promise((resolve, reject) => {
+        Cart.findOneAndUpdate(condition, updateData, { upsert: true }).then((result) => resolve()).then(error => reject(error))
+    })
+}
+
 exports.addItemToCart = (req, res) => {
     Cart.findOne({ user: req.user._id }).exec((error, cart) => {
         if (error) return res.status(400).json({ error: error });
         if (cart) {
+            let promiseArray = []
+            req.body.cartItems.forEach((cartItem) => {
+                const product = cartItem.product
+                const item = cart.cartItems.find(c => c.product == product)
+                let condition, update;
+                if (item) {
+                    condition = { "user": req.user._id, "cartItems.product": product }
+                    update = {
+                        "$set": {
+                            "cartItems.$": cartItem
+                        }
+                    }
+                }
+                else {
+                    condition = { user: req.user._id }
+                    update = {
+                        "$push": {
+                            "cartItems": cartItem
+                        }
+                    }
+                }
+                promiseArray.push(runUpdate(condition, update))
+            })
 
-            const isProductExists = cart.cartItems.find(c => c.product == req.body.cartItems.product);
-            if (isProductExists) {
-                Cart.findOneAndUpdate({ "user": req.user._id, "cartItems.product": req.body.cartItems.product }, {
-                    "$set": {
-                        "cartItems.$": { ...req.body.cartItems, quantity: isProductExists.quantity + req.body.cartItems.quantity }
-                    }
-                }).exec((error, _cart) => {
-                    if (error) return res.status(400).json({ error: error });
-                    if (_cart) return res.status(201).json({ cart: _cart });
-                });
-            } else {
-                //if user already added something in cart, update cart items
-                Cart.findOneAndUpdate({ user: req.user._id }, {
-                    "$push": {
-                        "cartItems": req.body.cartItems
-                    }
-                }).exec((error, _cart) => {
-                    if (error) return res.status(400).json({ error: error });
-                    if (_cart) return res.status(201).json({ cart: _cart });
-                });
-            }
+            Promise.all(promiseArray).then((response) => res.status(201).json({ response })).catch(error => res.status(400).json({ error }))
+
         } else {
             //if not exists create new
             const cart = new Cart({
                 user: req.user._id,
-                cartItems: [req.body.cartItems],
+                cartItems: req.body.cartItems,
             })
             cart.save((error, cart) => {
                 if (error) return res.status(400).json({ error: error });
@@ -40,3 +50,23 @@ exports.addItemToCart = (req, res) => {
 
     })
 }
+
+exports.getCartItems = ((req, res) => {
+    Cart.findOne({ user: req.user._id }).populate('cartItems.product', '_id,name, price,ProductPictures').exec((error, cart) => {
+        if (error) return res.status(400).json({ error: error });
+        if (cart) {
+            let cartItems = {}
+            cart.cartItems.forEach((item, index) => {
+                cartItems[item.product._id.toString()] = {
+                    _id: item.product._id.toString(),
+                    name: item.product.name,
+                    img: item.product.productPictures[0].img,
+                    price: item.product.price,
+                    qty: item.quantity
+                }
+
+            })
+            res.status(201).json({ cartItems });
+        }
+    })
+})
