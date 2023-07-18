7f2afc72342f7f13a18966343ea0406fe4a4d409
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-18 23:45:45 +0530

    cardHeader props handling

--- frontend/src/components/UI/Card/index.js
+++ frontend/src/components/UI/Card/index.js
@@ -3,6 +3,16 @@ import "./style.css"
 const Card = (props) => {
     return (
         <div className='card' {...props}>
+            <div className="cardHeader">
+                {
+                    props.headerLeft && <div>{props.headerLeft}</div>
+
+                }
+                {
+                    props.headerRight && props.headerRight
+
+                }
+            </div>
             {props.children}
         </div>
     )
