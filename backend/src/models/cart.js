e81d45c71e0f4dddec0f78c8258adf0da1b71fd1
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-22 23:28:34 +0530

    no need here :)

--- backend/src/models/cart.js
+++ backend/src/models/cart.js
@@ -16,11 +16,11 @@ const cartSchema = new mongoose.Schema({
             type: Number,
             default: 1
         },
-        price: {
-            type: Number,
-            default: 1,
-            required: true,
-        },
+        // price: {
+        //     type: Number,
+        //     default: 1,
+        //     required: true,
+        // },
     }]
 }, { timestamps: true })
 
