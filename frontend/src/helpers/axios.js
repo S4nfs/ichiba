7e45fbb25920ddf4ff84829c363a09179d8ae5f6
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-22 23:27:39 +0530

    intercept auth

--- frontend/src/helpers/axios.js
+++ frontend/src/helpers/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { api } from '../urlConfig';
+import store from '../store';
+
 const token = window.localStorage.getItem('token')
 const axiosInstance = axios.create({
     baseURL: api,
@@ -7,5 +9,14 @@ const axiosInstance = axios.create({
         'Authorization': token ? `Bearer ${token}` : ''
     }
 });
+axiosInstance.interceptors.request.use((req) => {
+    const { auth } = store.getState();
+    if (auth.token) {
+        req.headers.Authorization = `Bearer ${auth.token}`;
+    }
+    return req;
+})
 
 export default axiosInstance
+
+
