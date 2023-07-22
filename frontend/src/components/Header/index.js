32babfc9747bdd1bf1bed6a530bd7a8b3ad94200
S4nfs <zyzzaestheticsgods@gmail.com>
2023-07-22 23:30:32 +0530

    header styling refractored

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -96,43 +96,44 @@ const Header = (props) => {
                         </div>
                         <div className="rightspace">
 
+                            <div className="loginInputContainer">
 
-                            <MaterialInput
-                                type="text"
-                                label="Enter Email/Enter Mobile Number"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
+                                <MaterialInput
+                                    type="text"
+                                    label="Enter Email/Enter Mobile Number"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
 
-                            <MaterialInput
-                                type="password"
-                                label="Enter Password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                rightElement={<a href="#">Forgot?</a>}
-                            />
+                                <MaterialInput
+                                    type="password"
+                                    label="Enter Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    rightElement={<a href="#">Forgot?</a>}
+                                />
 
-                            <MaterialButton
-                                title="Login"
-                                bgColor="#fb641b"
-                                textColor="#ffffff"
-                                style={{
-                                    margin: '40px 0 20px 0'
-                                }}
-                                onClick={userLogin}
-                            />
-                            <p>
-                                OR
-                            </p>
-                            <MaterialButton
-                                title="Request OTP"
-                                bgColor="#ffffff"
-                                textColor="#2874f0"
-                                style={{
-                                    margin: '40px 0'
-                                }}
-                            />
-
+                                <MaterialButton
+                                    title="Login"
+                                    bgColor="#fb641b"
+                                    textColor="#ffffff"
+                                    style={{
+                                        margin: '40px 0 20px 0'
+                                    }}
+                                    onClick={userLogin}
+                                />
+                                <p style={{ textAlign: 'center' }}>
+                                    OR
+                                </p>
+                                <MaterialButton
+                                    title="Request OTP"
+                                    bgColor="#ffffff"
+                                    textColor="#2874f0"
+                                    style={{
+                                        margin: '40px 0'
+                                    }}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
